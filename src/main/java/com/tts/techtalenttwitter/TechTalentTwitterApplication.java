fc8504b2bb09056ab896e3370fbc8297ab393a06
Wes Lanning <lanning.52@gmail.com>
2019-12-02 18:22:59 -0500

    removed default security

--- src/main/java/com/tts/techtalenttwitter/TechTalentTwitterApplication.java
+++ src/main/java/com/tts/techtalenttwitter/TechTalentTwitterApplication.java
@@ -2,8 +2,9 @@ package com.tts.techtalenttwitter;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = SecurityAutoConfiguration.class)
 public class TechTalentTwitterApplication {
 
     public static void main(String[] args) {
